7debc02cb34b624f227fc2178dfe17cd3781452d
DarkShadowXx64 <gabo.garcia.osorio@gmail.com>
2023-10-07 20:53:14 -0500

    animaciones slidebar

--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import hamburguesa from "../Assets/hamburguesa.png";
 
 const App = () => {
   const [open, setOpen] = useState(true);
+  const [animationDelay, setAnimationDelay] = useState(open ? 0 : 1000);
   const Menus = [
     { title: "Home", gap: true },
     { title: "Perfiles", gap: true },
     { title: "Proyectos", gap: true },
     { title: "Explorar", gap: true },
   ];
+
+  useEffect(() => {
+    setAnimationDelay(open ? 0 : 200);
+  }, [open]);
 
   const toggleSidebar = () => {
     setOpen(!open);
@@ -19,7 +24,7 @@ const App = () => {
       <div
         className={`${
           open ? "w-60" : "w-20 "
-        } bg-dark-purple h-screen p-5  pt-6 relative duration-300`}
+        } bg-dark-purple h-screen p-5 pt-6 relative duration-500 transition-transform transform-gpu`}
       >
         <div className="flex gap-x-4 items-center">
           <button
@@ -31,7 +36,7 @@ const App = () => {
           <h1
             className={`${
               open
-                ? " text-white origin-left font-medium text-xl duration-200"
+                ? "text-white origin-left font-medium text-xl duration-200"
                 : "hidden"
             }`}
           >
@@ -42,14 +47,24 @@ const App = () => {
           {Menus.map((Menu, index) => (
             <li
               key={index}
-              className={`flex flex-col items-center rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm 
-              ${Menu.gap ? "mt-9" : "mt-2"} ${
-                index === 0 && "bg-light-white"
-              } `}
+              className={`flex flex-col items-center rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm transform ${
+                Menu.gap ? "mt-9" : "mt-2"
+              } ${index === 0 && "bg-light-white"}`}
+              style={{
+                transform: open ? "translateX(0)" : "translateX(-100%)",
+                opacity: open ? 1 : 0,
+                transitionDuration: open
+                  ? "400ms"
+                  : index === 0
+                  ? "1000ms"
+                  : "50ms",
+                transitionProperty: "transform, opacity",
+                transitionDelay: `${animationDelay + index * 100}ms`,
+              }}
             >
               <span
                 className={`${
-                  open ? "origin-left duration-200" : "hidden"
+                  open ? "origin-left duration-100" : "hidden"
                 }`}
               >
                 {Menu.title}
