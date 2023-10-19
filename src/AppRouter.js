2d25b2b14c14649e29ff51b31901539da04404d9
DarkShadowXx64 <gabo.garcia.osorio@gmail.com>
2023-10-18 21:43:10 -0500

    login y register

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,14 +1,21 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Inicio from './Pages/Inicio';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
 
 function AppRouter() {
-    return (
-        <>
-        <Routes>
-            <Route path ='/' element={<Inicio/>}/>
-        </Routes>
-        </>
-    )
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
 }
 
-export default AppRouter
+export default AppRouter;
