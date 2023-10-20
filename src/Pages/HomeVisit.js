eee1caa8a4afaee7e70d52a3d03dc4d9550d8b1b
Cana23 <ethanbarea@gmail.com>
2023-10-19 22:00:22 -0500

    Correciones de HomeVisit

--- src/Pages/HomeVisit.js
+++ src/Pages/HomeVisit.js
@@ -1,21 +1,38 @@
 import React from "react";
 import Footer from "../Components/Footer";
+import userIcon from "../Assets/Iconoperfil.png";
+import logoMorado from "../Assets/Logomorado.png";
 
 function HomeVisit() {
-  
+  const userList = [
+    { id: 1, name: "ProgramaPro", avatar: userIcon },
+    { id: 2, name: "CodeMaster", avatar: userIcon },
+    { id: 3, name: "DevGenius", avatar: userIcon },
+    { id: 4, name: "CodingNinja", avatar: userIcon },
+    { id: 5, name: "CodeExplorer", avatar: userIcon },
+    { id: 6, name: "BitWiz", avatar: userIcon },
+    { id: 7, name: "CodeSavvy", avatar: userIcon },
+    { id: 8, name: "DevGuru", avatar: userIcon },
+    { id: 9, name: "CodeEnthusiast", avatar: userIcon },
+    { id: 10, name: "ScriptingPro", avatar: userIcon },
+  ];
+
   return (
     <div className="min-h-screen flex flex-col">
       <header className="bg-gray-100 py-4">
         <div className="flex items-center justify-between px-4">
           <div className="flex items-center space-x-4 px-4">
             {/* Logo */}
-            <img src="../Assets/logoMorado.png" />
+            <img src={logoMorado} alt="Logo" className="w-14" />
             {/* Nombre de la plataforma */}
             <div>
-              <h1 className="text-xl font-semibold">Nombre de la Plataforma</h1>
-              <p className="text-xs text-gray-500">-Insertar eslogan-</p>
+              <h1 className="text-xl font-semibold">DevLink</h1>
+              <p className="text-xs text-gray-500">
+                Donde el código encuentra su comunidad
+              </p>
             </div>
           </div>
+
           {/* Botones de registro e inicio de sesión */}
           <ul className="flex items-center space-x-4">
             {/* Botón de Iniciar Sesión */}
@@ -67,19 +84,43 @@ function HomeVisit() {
       </header>
 
       <main className="flex flex-1">
-        <div className="w-1/3 p-4">
-          <div className="p-4">
+        <div className="w-1/4 h-full p-4">
+          <div className="p-4 border-r-2 border-gray-600">
             <h2 className="text-lg font-semibold mb-2">Usuarios Populares</h2>
-            <ul>
-              {/* Futura lista de usuarios populares */}
+            <ul className="space-y-2">
+              {userList.map((user) => (
+                <li key={user.id} className="flex items-center">
+                  <img
+                    src={user.avatar}
+                    alt={user.name}
+                    className="w-8 h-8 rounded-full mr-2"
+                  />
+                  {user.name}
+                </li>
+              ))}
             </ul>
           </div>
         </div>
 
-        <div className="w-2/3 p-4">
+        <div className="w-3/4 p-4">
           <div className="p-4">
-            <h2 className="text-lg font-semibold mb-2">Posts Mas Populares</h2>
-            <p className="text-gray-500">Los posts mas populares van aquí</p>
+            <h2 className="text-2xl font-bold mb-2">Posts Mas Populares</h2>
+            {/*Inicio del post*/}
+            <div
+              className="flex w-2/3 bg-white mx-12 rounded-md p-7 mb-6 flex-wrap"
+              style={{ height: "26rem" }}
+            >
+              <div className="h-14 w-14 bg-[#724DC5] rounded-full mr-4"></div>
+              <div className="text-left">
+                <h2 className="text-lg">Usuario</h2>
+                <p className="text-md">@Usuario</p>
+              </div>
+              <div className="w-full">
+                <p className="text-lg">Texto del Usuario</p>
+              </div>
+              <div className="w-full h-[68%] bg-[#724DC5] rounded-sm self-end"></div>
+            </div>
+            {/*Final del post*/}
           </div>
         </div>
       </main>
