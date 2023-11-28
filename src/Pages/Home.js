3ae2e72c9fc225133fb166744ed31922d29bb83f
DarkShadowXx64 <gabo.garcia.osorio@gmail.com>
2023-11-28 04:12:39 -0500

    mamadas del front

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,7 +4,7 @@ import Sidebar from '../Components/sidebar';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
-
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
 const Home = () => {
   const [news, setNews] = useState([]);
   const [publicaciones, setPublicaciones] = useState([]);
@@ -55,9 +55,9 @@ const Home = () => {
 
   const handleSubmitPublicaciones = async (e) => {
     e.preventDefault();
-    if (!datos.titulo || !datos.contenido) {
+    if (!datos.contenido) {
       setPublicacionError(
-        'Por favor, completa el título y la descripción para publicar.'
+        'Por favor, completa el campo para publicar.'
       );
       return;
     }
@@ -101,7 +101,7 @@ const Home = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          'https://newsapi.org/v2/everything?q=software&pageSize=6&sortBy=popularity',
+          'https://newsapi.org/v2/everything?q=software&pageSize=20&sortBy=popularity',
           {
             headers: {
               'X-Api-Key': '0ab8e04dbe1944d79178f1f971919ec0',
@@ -125,52 +125,52 @@ const Home = () => {
       <Sidebar />
       <div className="flex mt-0 flex-grow justify-center">
         <div className='p-10 w-full ml-36 mt-20'>
-          <div className="w-full flex flex-wrap mx-12 rounded-md p-4 pb-2 h-fit bg-gray-100 mb-4 mt-2"  
-          style={{
-                    boxShadow: '-5px 0 5px -5px rgba(0, 0, 0, 0.3), 5px 0 5px -5px rgba(0, 0, 0, 0.3), 0 5px 5px -5px rgba(0, 0, 0, 0.5)',
-                  }}>
+          <div className="w-full flex flex-wrap mx-12 rounded-md p-4 pb-2 h-fit bg-gray-100 mb-4 mt-2"
+            style={{
+              boxShadow: '-5px 0 5px -5px rgba(0, 0, 0, 0.3), 5px 0 5px -5px rgba(0, 0, 0, 0.3), 0 5px 5px -5px rgba(0, 0, 0, 0.5)',
+            }}>
             <h1 className='mb-1 font-semibold'>¿Qué estás pensando?</h1>
 
-            <form onSubmit={handleSubmitPublicaciones} className="w-full flex flex-wrap">
-              <input
-                placeholder="Título"
-                name="titulo"
-                value={datos.titulo}
-                onChange={(e) => setDatos({ ...datos, titulo: e.target.value })}
-                className="border border-gray-300 rounded-md px-3 py-2 mr-2 mb-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              />
+            <form onSubmit={handleSubmitPublicaciones} className="w-full flex flex-wrap items-start">
 
-              <input
-                placeholder="Descripción"
-                name="contenido"
-                value={datos.contenido}
-                onChange={(e) => setDatos({ ...datos, contenido: e.target.value })}
-                className="border border-gray-300 rounded-md px-3 py-2 mr-2 mb-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              />
-
+              <div className="w-full flex flex-wrap items-start mb-2">
                 <input
-                  type="file"
-                  accept="image/*"
-                  name="img"
-                  value={datos.img}
-                  onChange={handleImagenChange}
+                  placeholder="¿Qué estás pensando?"
+                  name="contenido"
+                  value={datos.contenido}
+                  onChange={(e) => setDatos({ ...datos, contenido: e.target.value })}
+                  className="border border-gray-300 rounded-md w-full px-3 py-2 mr-4 focus:outline-none focus:ring-2 focus:ring-indigo-500"
                 />
+                <label className="relative overflow-hidden">
+                  <span className="block py-2 px-2 rounded-md bg-gray-200 cursor-pointer mt-2 mr-4">
+                    Seleccionar archivo
+                  </span>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    name="img"
+                    value={datos.img}
+                    onChange={handleImagenChange}
+                    className="absolute inset-0 opacity-0 cursor-pointer "
+                  />
+                  {imageUrl && (
+                    <div className="mt-2">
+                      <img src={imageUrl} alt="Selected" className="max-w-full h-auto mr-4" /> {/* Clases para la imagen */}
+                    </div>
+                  )}
+                </label>
+              </div>
 
-              {imageUrl && (
-                <div className="mt-2">
-                  <img src={imageUrl} alt="Selected" className="max-w-full h-auto" />
-                </div>
+              {publicacionError && (
+                <p className="w-full text-red-600">{publicacionError}</p>
               )}
 
               <button
                 type="submit"
-                className="bg-indigo-500 hover:bg-indigo-700 text-white rounded-md px-8 py-2 mb-2 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition-colors duration-300"
+                className="ml-auto mt-auto bg-indigo-500 hover:bg-indigo-700 text-white rounded-md px-8 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition-colors duration-300"
               >
                 Publicar
               </button>
-              {publicacionError && (
-                <p className="mt-2 text-center text-red-600">{publicacionError}</p>
-              )}
             </form>
           </div>
           {publicaciones ? (
@@ -196,22 +196,22 @@ const Home = () => {
                       <p className='text-sm'>{publicacion.correo}</p>
                     </div>
 
-              <div >
-                <button><FontAwesomeIcon icon={faHeart} size="lg" style={{ color: "#ff0066", }} /> </button>
-              </div>
-            </div>
-            <div className='w-full'>
-              <p className='text-lg py-2'>{ publicacion.img ? (publicacion.titulo) : (publicacion.contenido) }</p>
-            </div>
-            
-              { publicacion.img && (
-                <div className='w-full h-96 bg-[#724DC5] rounded-md self-end'>
-                  <img src={`${backendBaseUrl}/${publicacion.img}`} className="object-cover w-full h-full rounded-md" alt="content"></img>
-                </div>
-             )}
-            
-            <div className='w-full'>
-              <form onSubmit= {(e) => handleSubmitComentarios(e)} >
+                    <div >
+                      <button><FontAwesomeIcon icon={faHeart} size="lg" style={{ color: "#ff0066", }} /> </button>
+                    </div>
+                  </div>
+                  <div className='w-full'>
+                    <p className='text-lg py-2'>{publicacion.contenido}</p>
+                  </div>
+
+                  {publicacion.img && (
+                    <div className='w-full h-96 bg-[#724DC5] rounded-md self-end'>
+                      <img src={`${backendBaseUrl}/${publicacion.img}`} className="object-cover w-full h-full rounded-md" alt="content"></img>
+                    </div>
+                  )}
+
+                  <div className='w-full'>
+                    <form onSubmit={(e) => handleSubmitComentarios(e)} >
 
                       <input
                         className='border-2 rounded-md w-[91%] mt-2 px-2 text-sm'
@@ -223,12 +223,10 @@ const Home = () => {
                       ></input>
 
 
-                      <Link to="/comentar">
                         <button>
-                          <FontAwesomeIcon icon={faComment} className='pl-1' size="lg" style={{ color: "#5D30C1" }} />
+                          <FontAwesomeIcon icon={faPaperPlane} className='pl-1' size="lg" style={{ color: "#5D30C1" }} />
                         </button>
-                      </Link>
-                      <button type='submit'>submit</button>
+  
                     </form>
                   </div>
 
@@ -240,7 +238,7 @@ const Home = () => {
             <p>Loading...</p>
           )}
         </div>
-            
+
 
 
 
